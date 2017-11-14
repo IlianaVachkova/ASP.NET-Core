e7fc265d9979b6977a312261316bb9005606f2b9
Iliana Vachkova <an_vachkova@yahoo.com>
2017-11-14 23:42:38 +0200

    Added database models

---
+++ ASP.NET Core - Essentials/CarDealer.Web/CarDealer.Data/Models/Sale.cs
@@ -0,0 +1,13 @@
+﻿namespace CarDealer.Data.Models
+{
+    public class Sale
+    {
+        public int Id { get; set; }
+
+        public virtual Car Car { get; set; }
+
+        public virtual Customer Customer { get; set; }
+
+        public double Discount { get; set; }
+    }
+}
