08e0ce9dc3a689bced600889736ab34b22c762c3
Iliana Vachkova <an_vachkova@yahoo.com>
2017-12-24 18:51:37 +0200

    Added EF Core homework

---
+++ Introduction to .Net Core and EF Core/Shop/Shop/Data/ShopDbContext.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Shop.Data
+{
+    public class ShopDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Salesman> Salesmen { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<Review> Reviews { get; set; }
+
+        public DbSet<Item> Items { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlServer("Server=.\\SQLEXPRESS;Database=TestShopApp;Integrated security=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder
+                .Entity<Customer>()
+                .HasOne(c => c.Salesman)
+                .WithMany(s => s.Customers)
+                .HasForeignKey(c => c.SalesmanId);
+
+            builder
+                .Entity<Order>()
+                .HasOne(o => o.Customer)
+                .WithMany(c => c.Orders)
+                .HasForeignKey(o => o.CustomerId);
+
+            builder
+                .Entity<Review>()
+                .HasOne(r => r.Customer)
+                .WithMany(c => c.Reviews)
+                .HasForeignKey(r => r.CustomerId);
+
+            builder
+                .Entity<Review>()
+                .HasOne(r => r.Item)
+                .WithMany(i => i.Reviews)
+                .HasForeignKey(r => r.ItemId);
+
+            builder
+                .Entity<OrderItem>()
+                .HasKey(oi => new { oi.ItemId, oi.OrderId });
+
+            builder
+                .Entity<Item>()
+                .HasMany(o => o.Orders)
+                .WithOne(oi => oi.Item)
+                .HasForeignKey(oi => oi.ItemId);
+
+            builder
+                .Entity<Order>()
+                .HasMany(i => i.Items)
+                .WithOne(oi => oi.Order)
+                .HasForeignKey(oi => oi.OrderId);
+        }
+    }
+}
